413d8bee5a44b3fc1a8047978c93591f9ffdf865
Perfect Gentleman <perfect007gentleman@gmail.com>
2020-04-20 20:11:57 +0700

    Update nagastart.sh

--- nagastart.sh
+++ nagastart.sh
@@ -5,7 +5,7 @@
 # xinput set-int-prop $NAGAID1 "Device Enabled" 8 0
  
 # Insanity, just because naga 2014
-NAGAID2=$(xinput | grep Naga | grep pointer | cut -d= -f2 | cut -f1)
+NAGAID2=$(xinput | grep xwayland | grep pointer | cut -d= -f2 | cut -f1)
 if [[ `echo $NAGAID2 | wc -w` -eq 2 ]]; then
 if [[ `xinput get-button-map $(echo $NAGAID2 | awk '{print $1}') | grep 10 | wc -l` -eq 1 ]]; then
 xinput set-button-map $(echo $NAGAID2 | awk '{print $1}') 1 2 3 4 5 6 7 11 10 8 9 13 14 15
